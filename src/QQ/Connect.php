04fa50252a4d8aa2104731423e3c9fccd00ca145
hua <yuchonghua@163.com>
2017-09-27 18:43:27 +0800

    Add. setConfig

--- src/QQ/Connect.php
+++ src/QQ/Connect.php
@@ -4,8 +4,29 @@ namespace DdvPhp\QQ;
 class Connect extends \DdvPhp\QQ\Connect\QC
 {
   // 魔术方法
-  public function __construct()
+  public function __construct($config = null)
   {
     parent::__construct();
+    isset($config) && is_array($config) && $this->setConfig($config);
+  }
+  public function setConfig($config = null){
+  	if (!is_array($config)) {
+  		return;
+  	}
+  	if (!empty($config['appid'])) {
+  		$this->setAppId($appid);
+  	}
+  	if (!empty($config['scope'])) {
+  		$this->setScope($scope);
+  	}
+  	if (!empty($config['openid'])) {
+  		$this->setOpenid($openid);
+  	}
+  	if (!empty($config['callback'])) {
+  		$this->setCallbackUrl($callback);
+  	}
+  	if (!empty($config['access_token'])) {
+  		$this->setAccessToken($access_token);
+  	}
   }
 }
